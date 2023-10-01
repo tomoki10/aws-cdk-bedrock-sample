441042e9c93c8351657fba864607376f717792ee
Tomoki Sato <sato.tomoki@HL01352.local>
2023-10-01 17:05:19 +0900

    Initial commit

---
+++ test/aws-cdk-bedrock-sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkBedrockSample from '../lib/aws-cdk-bedrock-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-bedrock-sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkBedrockSample.AwsCdkBedrockSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
